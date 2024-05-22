cb067fb19c343ced5863b72430f3dfeae3bb8fe4
poojaedunet <122346906+poojaedunet@users.noreply.github.com>
2024-05-22 09:46:50 +0530

    Add files via upload

---
+++ Primitive.java
@@ -0,0 +1,65 @@
+public class Primitive {
+     public static void main(String[] args) {
+
+   // byte
+  
+        byte b = 100;
+        System.out.println("value of byte is: " + b);
+    
+
+    //int
+
+   
+        int i = 10;
+        System.out.println("Integer value of i  is: " + i);
+    
+    
+        //short
+
+    short s = -10000;
+    System.out.println(" value of short is: " + s);
+
+    //long
+
+        long l = 10000000000L;
+        System.out.println(" value of long  is: " + l);
+   
+
+   
+  
+
+    //char
+  
+        char c = 'A';
+        System.out.println("char value is: " + c);
+   
+
+
+
+
+//float
+   
+        float f = 10;
+        System.out.println(" value of float  is: " + f);
+
+    
+//double
+ 
+        double d = 40.5;
+        System.out.println("value of double is: " + d);
+
+
+ //boolean
+  
+        boolean bl = true;
+        System.out.println(bl);
+
+ 
+
+
+
+}
+}
+
+
+
